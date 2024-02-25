0560e34c49a36a97a8504bb6c1f0dec4865046fe
LiubovO <143088061+LiubovO@users.noreply.github.com>
2024-02-25 16:40:53 +0300

    Add files via upload
    git не коннектится с github

---
+++ sprint/perevalapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Users, Coords, PerevalAdded, Images
+
+admin.site.register(Users)
+admin.site.register(Coords)
+admin.site.register(PerevalAdded)
+admin.site.register(Images)
---
+++ sprint/perevalapp/views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from rest_framework import generics
+from .serializers import *
+
+
+class PerevalAddedAPI(generics.CreateAPIView):
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalAddedSerializer
+
+    def post(self, request):
+        pereval = PerevalAddedSerializer(data=request.data)
+        try:
+            if pereval.is_valid(raise_exception=True):
+                pereval.save()
+                data = {'status': '200', 'message': 'null', 'id': f'{pereval.instance.id}'}
+                return JsonResponse(data, status=200, safe=False)
+
+        except Exception as exc:
+            responsedata = {'status': '400', 'message': f'Bad Request: {exc}', 'id': 'null'}
+            return JsonResponse(responsedata, status=400, safe=False)
+
